d57cb0d707f2025c55ee7ab8d2f6d3e67e35dd9a
Matteo DE VITA <matteo.de-vita@epitech.eu>
2020-10-17 17:22:48 +0200

    Write simple asm loader

---
+++ loader.s
@@ -0,0 +1,15 @@
+global loader ;entry symbol for ELF
+
+MAGIC_NUMBER equ 0x1BADB002 ;magic number constant
+FLAG equ 0x0
+CHEKSUM equ -MAGIC_NUMBER ;compute control sum (MAGIC_NUMBER + FLAG must be == 0)
+
+section .text:
+align 4 ;code must be aligned on 4 bytes
+    dd MAGIC_NUMBER ;dd write values in machine code
+    dd FLAG
+    DD CHEKSUM
+.loader:
+    mov eax 0xCAFEBABE ;mov 0xCAFEBABE to eax register
+.loop:
+    jmp loop ;infinite loop
